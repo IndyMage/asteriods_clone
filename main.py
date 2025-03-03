11277c210ae8107fb8c78c0c0c948ac6fdf2a75c
IndyMage <edwardhorner@gmail.com>
2025-03-03 14:56:17 -0800

    cleaned main

--- main.py
+++ main.py
@@ -16,11 +16,5 @@ def main():
         screen.fill((BLACK))
         pygame.display.flip()
 
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 if __name__ == "__main__":
     main()
